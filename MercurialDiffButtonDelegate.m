bf717b8618d3ab847c3851ea4363297bb7a7a809
Daniel Grace <dgrace@busmetsoftware.com>
2009-12-13 18:06:08 -0600

    Fix mercurial locking bug.

--- MercurialDiffButtonDelegate.m
+++ MercurialDiffButtonDelegate.m
@@ -76,63 +76,74 @@
 	[NSApp hide: self];
 }
 
+- (void) setAllTitles: (NSString *)s {
+	[s retain];
+	dispatch_async(dispatch_get_main_queue(), ^{
+		[lock lock];
+		[s autorelease];
+		[self setTitle: s];
+		[self setShortTitle: s];
+		[lock unlock];
+	});
+}
+
 - (void) fire {
-	[lock lock];
-	NSTask *t = [[NSTask alloc] init];
-	NSString *lp = [NSString stringWithFormat: @"%s", hg];
-	[t setLaunchPath: lp];
-	[t setCurrentDirectoryPath: repository];
-	[t setArguments: [NSArray arrayWithObjects: @"diff", nil]];
-
-	NSPipe *pipe = [NSPipe pipe];
-	[t setStandardOutput: pipe];
+	dispatch_async(dispatch_get_global_queue(0, 0), ^{
+		[lock lock];
+		NSTask *t = [[NSTask alloc] init];
+		NSString *lp = [NSString stringWithFormat: @"%s", hg];
+		[t setLaunchPath: lp];
+		[t setCurrentDirectoryPath: repository];
+		[t setArguments: [NSArray arrayWithObjects: @"diff", nil]];
 	
-	NSTask *t2 = [[NSTask alloc] init];
-	[t2 setLaunchPath: @"/usr/bin/diffstat"];
-	[t2 setStandardInput: pipe];
+		NSPipe *pipe = [NSPipe pipe];
+		[t setStandardOutput: pipe];
+		
+		NSTask *t2 = [[NSTask alloc] init];
+		[t2 setLaunchPath: @"/usr/bin/diffstat"];
+		[t2 setStandardInput: pipe];
+		
+		NSPipe *pipe2 = [NSPipe pipe];
+		[t2 setStandardOutput: pipe2];
+		
+		NSFileHandle *file = [pipe2 fileHandleForReading];
 	
-	NSPipe *pipe2 = [NSPipe pipe];
-	[t2 setStandardOutput: pipe2];
+		NSArray *branches = [self arrayFromResultOfArgs: [NSArray arrayWithObjects: @"branches", nil]];
 	
-	NSFileHandle *file = [pipe2 fileHandleForReading];
-
-	NSArray *branches = [self arrayFromResultOfArgs: [NSArray arrayWithObjects: @"branches", nil]];
-
-	NSMenu *m = [[NSMenu alloc] initWithTitle: @"Testing"];
-	[m insertItemWithTitle: @"Branches" action: @selector(branch:) keyEquivalent: @"" atIndex: 0];
-	[m insertItem: [NSMenuItem separatorItem] atIndex: 1];
-
-	int i;
-	int the_index = 2;
-	for (i = 0; i < [branches count]; i++) {
-		NSString *tmp = [branches objectAtIndex: i];
-		if (tmp && [tmp length] > 0) {
-			[m insertItemWithTitle: tmp action: nil keyEquivalent: @"" atIndex: the_index++];
-			if ('*' == [tmp characterAtIndex: 0]) {
-				tmp = [tmp stringByTrimmingCharactersInSet: [NSCharacterSet characterSetWithCharactersInString: @" \n*\r"]];
-			} else {
-				tmp = [tmp stringByTrimmingCharactersInSet: [NSCharacterSet characterSetWithCharactersInString: @" \n*\r"]];
+		NSMenu *m = [[NSMenu alloc] initWithTitle: @"Testing"];
+		[m insertItemWithTitle: @"Branches" action: @selector(branch:) keyEquivalent: @"" atIndex: 0];
+		[m insertItem: [NSMenuItem separatorItem] atIndex: 1];
+	
+		int i;
+		int the_index = 2;
+		for (i = 0; i < [branches count]; i++) {
+			NSString *tmp = [branches objectAtIndex: i];
+			if (tmp && [tmp length] > 0) {
+				[m insertItemWithTitle: tmp action: nil keyEquivalent: @"" atIndex: the_index++];
+				if ('*' == [tmp characterAtIndex: 0]) {
+					tmp = [tmp stringByTrimmingCharactersInSet: [NSCharacterSet characterSetWithCharactersInString: @" \n*\r"]];
+				} else {
+					tmp = [tmp stringByTrimmingCharactersInSet: [NSCharacterSet characterSetWithCharactersInString: @" \n*\r"]];
+				}
 			}
 		}
-	}
-
-	[m insertItemWithTitle: @"" action: nil keyEquivalent: @"" atIndex: the_index++];
-	[m insertItemWithTitle: @"Logs" action: nil keyEquivalent: @"" atIndex: the_index++];
-	[m insertItem: [NSMenuItem separatorItem] atIndex: the_index++];
 	
-	NSArray *logs = [self arrayFromResultOfArgs: [NSArray arrayWithObjects: @"log", @"--template", @"{node|short} {desc}\n", nil]];
-	for (i = 0; i < [logs count]; i++) {
-		NSString *tmp = [logs objectAtIndex: i];
-		if (tmp && [tmp length] > 0) {
-			[m insertItemWithTitle: tmp action: nil keyEquivalent: @"" atIndex: the_index++];
+		[m insertItemWithTitle: @"" action: nil keyEquivalent: @"" atIndex: the_index++];
+		[m insertItemWithTitle: @"Logs" action: nil keyEquivalent: @"" atIndex: the_index++];
+		[m insertItem: [NSMenuItem separatorItem] atIndex: the_index++];
+		
+		NSArray *logs = [self arrayFromResultOfArgs: [NSArray arrayWithObjects: @"log", @"--template", @"{node|short} {desc}\n", nil]];
+		for (i = 0; i < [logs count]; i++) {
+			NSString *tmp = [logs objectAtIndex: i];
+			if (tmp && [tmp length] > 0) {
+				[m insertItemWithTitle: tmp action: nil keyEquivalent: @"" atIndex: the_index++];
+			}
 		}
-	}
-	
-	[m insertItemWithTitle: @"" action: nil keyEquivalent: @"" atIndex: the_index++];
-	[m insertItemWithTitle: @"Actions" action: nil keyEquivalent: @"" atIndex: the_index++];
-	[m insertItem: [NSMenuItem separatorItem] atIndex: the_index++];
+		
+		[m insertItemWithTitle: @"" action: nil keyEquivalent: @"" atIndex: the_index++];
+		[m insertItemWithTitle: @"Actions" action: nil keyEquivalent: @"" atIndex: the_index++];
+		[m insertItem: [NSMenuItem separatorItem] atIndex: the_index++];
 
-	dispatch_async(dispatch_get_global_queue(0, 0), ^{
 		[lock lock];
 		[t autorelease];
 		[t2 autorelease];
@@ -144,39 +155,31 @@
 		NSCharacterSet *cs = [NSCharacterSet whitespaceAndNewlineCharacterSet];
 		NSString *utf8String = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
 		NSString *string = [[utf8String stringByTrimmingCharactersInSet: cs] retain];
+
+		NSArray *arr = [string componentsSeparatedByString: @"\n"];
+		NSString *s2 = [arr objectAtIndex: [arr count] - 1];
+		s2 = [s2 stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	
+		if (![s2 isEqual: @"0 files changed"]) {
+			localMod = YES;
+			[[m insertItemWithTitle: @"Commit these changes" action: @selector(commit:) keyEquivalent: @"" atIndex: the_index + 1] setTarget: self];
+			NSString *sTit = [NSString stringWithFormat: @"%@: %@", [repository lastPathComponent], s2];
+		
+			[self setAllTitles: sTit];
+		} else {
+			localMod = NO;
+			NSString *sTit = [NSString stringWithFormat: @"hg: %@",
+				[repository lastPathComponent]];
+
+			[self setAllTitles: sTit];
+		}
+		[lock unlock];
 		dispatch_async(dispatch_get_main_queue(), ^{
 			[lock lock];
-			[string autorelease];
-
-			NSArray *arr = [string componentsSeparatedByString: @"\n"];
-			NSString *s2 = [arr objectAtIndex: [arr count] - 1];
-			s2 = [s2 stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
-		
-			if (![s2 isEqual: @"0 files changed"]) {
-				localMod = YES;
-//				[[m insertItemWithTitle: @"Commit these changes" action: @selector(commit:) keyEquivalent: @"" atIndex: the_index + 1] setTarget: self];
-				NSString *sTit = [NSString stringWithFormat: @"%@: %@", [repository lastPathComponent], s2];
-			
-				[self setTitle: sTit];
-				[self setShortTitle: sTit];
-			} else {
-				localMod = NO;
-				NSString *sTit = [NSString stringWithFormat: @"hg: %@",
-					[repository lastPathComponent]];
-
-				[self setTitle: sTit];
-				[self setShortTitle: sTit];
-			}
+			[menuItem setSubmenu: m];
 			[lock unlock];
 		});
-		[lock unlock];
 	});
-	dispatch_async(dispatch_get_main_queue(), ^{
-		[lock lock];
-		[menuItem setSubmenu: m];
-		[lock unlock];
-	});
-	[lock unlock];
 }
 
 @end
